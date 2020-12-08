1d7a0dc8b329f8e4f35d1bd28733c39e1e11b039
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2020-12-08 15:21:18 +0100

    Added class KeyboardInput.java

---
+++ sp5/src/KeyboardInput.java
@@ -0,0 +1,104 @@
+public class KeyboardInput {
+    boolean wDown = false;
+    boolean aDown = false;
+    boolean sDown = false;
+    boolean dDown = false;
+
+    boolean upArrow = false;
+    boolean leftArrow = false;
+    boolean downArrow = false;
+    boolean rightArrow = false;
+
+    public KeyInput() {
+
+        boolean wDown ()
+        {
+            return wDown;
+        }
+
+        public boolean aDown ()
+        {
+            return aDown;
+        }
+
+        public boolean sDown ()
+        {
+            return sDown;
+        }
+
+        public boolean dDown ()
+        {
+            return dDown;
+        }
+
+
+        public boolean upArrow ()
+        {
+            return upArrow;
+        }
+
+        public boolean leftArrow ()
+        {
+            return leftArrow;
+        }
+
+        public boolean downArrow ()
+        {
+            return downArrow;
+        }
+
+        public boolean rightArrow ()
+        {
+            return rightArrow;
+        }
+    }
+
+
+    public void onKeyPressed(char ch) {
+        if (ch == 'W' || ch == 'w') {
+            wDown = true;
+        } else if (ch == 'A' || ch == 'a') {
+            aDown = true;
+        } else if (ch == 'S' || ch == 's') {
+            sDown = true;
+        } else if (ch == 'D' || ch == 'd') {
+            dDown = true;
+        }
+    }
+
+    public void onKeyCodePressed() {
+        if (keyCode == UP) {
+            upArrow = true;
+        } else if (keyCode == LEFT) {
+            leftArrow = true;
+        } else if (keyCode == DOWN) {
+            downArrow = true;
+        } else if (keyCode == RIGHT) {
+            rightArrow = true;
+        }
+    }
+
+    public void onKeyReleased(char ch) {
+        if (ch == 'W' || ch == 'w') {
+            wDown = false;
+        } else if (ch == 'A' || ch == 'a') {
+            aDown = false;
+        } else if (ch == 'S' || ch == 's') {
+            sDown = false;
+        } else if (ch == 'D' || ch == 'd') {
+            dDown = false;
+        }
+    }
+
+    public void onKeyCodeReleased() {
+        if (keyCode == UP) {
+            upArrow = false;
+        } else if (keyCode == LEFT) {
+            leftArrow = false;
+        } else if (keyCode == DOWN) {
+            downArrow = false;
+        } else if (keyCode == RIGHT) {
+            rightArrow = false;
+        }
+    }
+}
