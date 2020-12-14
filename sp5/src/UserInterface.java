5bf42f075926be4c365c40099cff7e3bcaf57f7c
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2020-12-14 09:30:43 +0100

    Added Class: UserInterface (UI)
    Title.

---
+++ sp5/src/UserInterface.java
@@ -0,0 +1,25 @@
+import processing.core.PApplet;
+
+public class UserInterface extends PApplet {
+
+    public void ui() {
+        fill(255);
+        textSize(25);
+//        if (Player.getPlayer1Life() > 0) {                                        Kan ikke få det til at virke helt.
+//            text("Player 1 Lives: " + Player.getPlayer1Life(), 25, 25);
+//        }
+//        if (Player.getPlayer2Life() > 0) {
+//            text("Player 2 Lives: " + Player.getPlayer2Life(), 25, 50);
+//        }
+//
+//        if (Player.getPlayer1Life() <= 0) {
+//            fill(255);
+//            text("Player 2 wins!", width / 2, height / 2);
+//            textSize(100);
+//        } else if (Player.getPlayer2Life() <= 0) {
+//            fill(255);
+//            text("Player 1 wins!", width / 2, height / 2);
+//            textSize(100);
+//        }
+    }
+}
