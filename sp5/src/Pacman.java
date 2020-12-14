e883bce334ff978d6dd285197f090fca2de3813a
ErminDZ <71074936+ErminDZ@users.noreply.github.com>
2020-12-14 18:25:43 +0100

    update

--- sp5/src/Pacman.java
+++ sp5/src/Pacman.java
@@ -23,7 +23,7 @@ public class Pacman extends PApplet {
         int ghostColorOffset = 3;
         for (int i =0; i < ghosts.length; i++)
         {
-          //  ghosts[i] = new Ghost(i + ghostColorOffset, p); // TODO: Fix the value of the board so it correctly fits to each ghost color.
+//          ghosts[i] = new Ghost(i + ghostColorOffset, p); // TODO: Fix the value of the board so it correctly fits to each ghost color.
 
         }
     }
@@ -34,11 +34,11 @@ public class Pacman extends PApplet {
         b.clearBoard();
 
         // Update Ghosts
-        /*for (int i = 0; i < ghosts.length; i++)
+        for (int i = 0; i < ghosts.length; i++)
         {
-            ghosts[i].update();
-            b.setBoardValue(ghosts[i].dot.getX(),ghosts[i].dot.getY(), ghosts[i].getBoardValue());
-        }*/
+//            ghosts[i].update();
+//            b.setBoardValue(ghosts[i].dot.getX(),ghosts[i].dot.getY(), ghosts[i].getBoardValue());
+        }
 
         // Update Player
 
