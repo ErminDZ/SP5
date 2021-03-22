0c47d390fa4f55dbf767bc817554b363916fddc0
Ermin Dzafic <71074936+ErminDZ@users.noreply.github.com>
2021-03-22 11:20:52 +0100

    ggh

--- sp5/src/Background.java
+++ sp5/src/Background.java
@@ -15,9 +15,7 @@ public class Background {
         this.p = p;
         this.board = getStaticMap(); //NY
 
-
     }
-
 
     public int[][] getBoard() {
         return this.board;
@@ -27,7 +25,6 @@ public class Background {
         this.board = getStaticMap(); // NY
 
     }
-
 
     public void setBoardValue(int x, int y, int value) {
         if (x > 25)
@@ -40,38 +37,19 @@ public class Background {
         for (int y = 0; y < board.length; y++) {
             for (int x = 0; x < board.length; x++) {
 
-                if (board[x][y] == 0)
-                {
+                if (board[x][y] == 0) {
                     p.fill(0, 0, 255);// WALLS/blue
-                }
-
-                else if (board[x][y] == 1)
-                {
+                } else if (board[x][y] == 1) {
                     p.fill(0, 0, 0);// PATH/black
-                }
-
-                else if (board[x][y] == 2)
-                {
+                } else if (board[x][y] == 2) {
                     p.fill(255, 255, 0); //Yellow/player
-                }
-
-                else if (board[x][y] == 3)
-                {
+                } else if (board[x][y] == 3) {
                     p.fill(111, 111, 111);// ghost spawn area/gray
-                }
-
-                else if (board[x][y] == 4)
-                {
+                } else if (board[x][y] == 4) {
                     p.fill(255, 0, 0);// ghost/RED
-                }
-
-                else if (board[x][y] == 5)
-                {
+                } else if (board[x][y] == 5) {
                     p.fill(255, 0, 255);// ghost/purple
-                }
-
-                else if (board[x][y] == 6)
-                {
+                } else if (board[x][y] == 6) {
                     p.fill(0, 255, 255);// ghost/LIGHT BLUE
                 }
 
@@ -83,8 +61,6 @@ public class Background {
                 p.rect(x * 40, y * 40, 40, 40);
             }
         }
-
-
     }
 
     public int[][] getStaticMap() {
@@ -115,7 +91,7 @@ public class Background {
 
     public boolean canMove(int x, int y, int deltaX, int deltaY) {
         // Delta x or y is the value that we want to move. e.g. deltaX == 1, is +1 to x coordinate.
-        if (board[x + deltaX][y + deltaY] == 0 || board[x + deltaX][y + deltaY] == 3 ) {
+        if (board[x + deltaX][y + deltaY] == 0) {
             return false;
         }
         return true;
@@ -127,8 +103,7 @@ public class Background {
 
                 if (board[x][y] == 1) { // food
                     p.fill(0, 255, 0);
-
-
+                    
                     p.stroke(100, 100, 100);
                     p.ellipse(x * 40 + 20, y * 40 + 20, 10, 10); // 20 is half the square (offset)
                 }
